75297a15514c9d05f170932389aeb5d9d345a0af
eschulz2 <eschulz2@gmail.com>
2016-06-07 21:20:37 -0500

    check inventory before sale

--- app/controllers/charges_controller.rb
+++ app/controllers/charges_controller.rb
@@ -9,29 +9,37 @@ class ChargesController < ApplicationController
     @description = params[:description]
     @table_id = params[:table_id]
     @email = params[:stripeEmail]
-
-    customer = Stripe::Customer.create(
-      :email => @email,
-      :source  => params[:stripeToken]
-    )
-
-    charge = Stripe::Charge.create(
-      :customer    => customer.id,
-      :amount      => @amount,
-      :description => @description,
-      :currency    => 'usd',
-      :receipt_email => @email
-    )
-
     table = Table.find(@table_id)
-    table.purchased = true
-    table.save
-    flash[:notice] = "Booked!"
-    redirect_to :root
 
-  rescue Stripe::CardError => e
-    flash[:error] = e.message
-    redirect_to :root
+    if table.purchased == false
+
+      customer = Stripe::Customer.create(
+        :email => @email,
+        :source  => params[:stripeToken]
+      )
+
+      charge = Stripe::Charge.create(
+        :customer    => customer.id,
+        :amount      => @amount,
+        :description => @description,
+        :currency    => 'usd',
+        :receipt_email => @email
+      )
+
+      table.purchased = true
+      table.save
+      flash[:notice] = "Booked!"
+      redirect_to :root
+
+    else
+      flash[:alert] = "Failure since that spot was just sold moments ago!"
+      redirect_to :root
+    end
+
+    rescue Stripe::CardError => e
+      flash[:error] = e.message
+      redirect_to :root
+
   end
 
 end
